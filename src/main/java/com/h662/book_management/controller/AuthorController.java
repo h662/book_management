43be2ca61a1d9790c8735268e142e79423b1cd6e
h662 <h662hong@gmail.com>
2025-07-11 09:58:36 +0900

    first commit

---
+++ src/main/java/com/h662/book_management/controller/AuthorController.java
@@ -0,0 +1,35 @@
+package com.h662.book_management.controller;
+
+import com.h662.book_management.dto.AuthorDto;
+import com.h662.book_management.model.Author;
+import com.h662.book_management.service.AuthorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+@RequiredArgsConstructor
+public class AuthorController {
+    private final AuthorService authorService;
+
+    @GetMapping
+    public List<Author> list() {
+        return authorService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Author get(@PathVariable Integer id) {
+        return authorService.getById(id);
+    }
+
+    @PostMapping
+    public Author create(@Valid @RequestBody AuthorDto authorDto) {
+        Author author = new Author();
+        author.setName(authorDto.getName());
+
+        return authorService.create(author);
+    }
+}
---
+++ src/main/java/com/h662/book_management/service/AuthorService.java
@@ -0,0 +1,25 @@
+package com.h662.book_management.service;
+
+import com.h662.book_management.model.Author;
+import com.h662.book_management.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public List<Author> getAll() { return authorRepository.findAll(); }
+
+    public Author getById(Integer id) {
+        return authorRepository.findById(id).orElseThrow(() -> new NoSuchElementException("저자 없음"));
+    }
+
+    public Author create(Author author) {
+        return authorRepository.save(author);
+    }
+}
